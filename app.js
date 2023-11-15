ecf1485ee440b2422b162cc4b9648ba0ab3f4f54
Osvaldo Matias Corneli Nassif <122895353+OsvaldoCorneli@users.noreply.github.com>
2023-11-15 11:02:57 -0300

    finalizando proyecto clase 22 modulo 4

--- app.js
+++ app.js
@@ -12,3 +12,6 @@ app.get("/",(req, res)=>{
     res.sendFile(path.resolve(__dirname,"./views/home.html"))
 })
  
+app.get("/prueba",(req, res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/crearProducto.html"))
+})
