65d5d6ae33e09c785100d71e1867e893b411461a
Osvaldo Matias Corneli Nassif <122895353+OsvaldoCorneli@users.noreply.github.com>
2023-11-15 18:08:01 -0300

    agregue algunas cosas

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.listen(PORT, ()=>{
 app.use(express.static(path.resolve(__dirname,'./public')))
 
 app.get("/",(req, res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/home.html"))
+    res.sendFile(path.resolve(__dirname,"./views/home2.html"))
 })
  
 app.get("/prueba",(req, res)=>{
