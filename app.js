a3a583283f00f6783f6f0e4e05d47a394c963b70
Osvaldo Matias Corneli Nassif <122895353+OsvaldoCorneli@users.noreply.github.com>
2023-11-18 20:24:56 -0300

    completando mercadoliebre

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.listen(PORT, ()=>{
 app.use(express.static(path.resolve(__dirname,'./public')))
 
 app.get("/",(req, res)=>{
-    res.sendFile(path.resolve(__dirname,"./views/home2.html"))
+    res.sendFile(path.resolve(__dirname,"./views/home.html"))
 })
  
 app.get("/prueba",(req, res)=>{
